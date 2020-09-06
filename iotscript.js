b890ee17dede0bcd9df4cbe027a331a77ead3235
PrekshaNaik <51022468+PrekshaNaik@users.noreply.github.com>
2020-09-06 09:22:53 +0530

    Add files via upload

---
+++ iotscript.js
@@ -0,0 +1,11 @@
+  
+confirm("You will be taken to clicked link. Confirm?")
+function myFunction() {
+  var txt;
+  if (confirm("Press a button!")) {
+    txt = "You pressed OK!";
+  } else {
+    txt = "You pressed Cancel!";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
